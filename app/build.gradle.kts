16193a14504abc817382a7b9183d7a95f34afb6c
jubo445 <juboraj335.ja@gmail.com>
2025-07-23 19:56:18 +0600

    dagger hilt added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.google.gms.google.services)
+    alias(libs.plugins.android.hilt.dagger)
+    kotlin("kapt")
 }
 
 android {
@@ -66,4 +68,9 @@ dependencies {
 
     //lottie
     implementation(libs.lottie)
+
+    // hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
 }
